b3d883b08e54d8bb1d4938da2b69fb37175ceec6
jbiland123 <jeremias.biland@gmail.com>
2024-02-05 09:05:30 +0100

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_02_132054) do
+ActiveRecord::Schema.define(version: 2024_02_02_134040) do
+
+  create_table "products", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
