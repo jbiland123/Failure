7cd2f339712b21b9876494fd7964b11441cc66ff
jbiland123 <jeremias.biland@gmail.com>
2024-02-09 08:22:10 +0100

    start

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'jbuilder', '~> 2.7'
 
 # User Management gem
 gem 'devise', '~> 4.9', '>= 4.9.3'
+gem "devise_ldap_authenticatable"
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :ldap_authenticatable, :registerable,
+         :recoverable, :validatable, authentication_keys: [:username]
+
+  # Wenn die Remember me Checkbox noch benötigt wird:
+  #devise :ldap_authenticatable, :registerable,
+  #:recoverable, :validatable, :rememberable, authentication_keys: [:username]
 end
